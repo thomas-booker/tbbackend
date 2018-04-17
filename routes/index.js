3b320d2bce9af5a3d76bd6055b07c344e3d35517
Tom Booker <30446949+thomas-booker@users.noreply.github.com>
2018-04-17 07:42:10 +0100

    Added testpage.jade and a .get route to it for testing purposes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/skills', function(req, res, next) {
+  res.render('testpage', {message: 'It worked!'});
+});
+
+
+
 module.exports = router;
